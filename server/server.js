b446773c754c71772740b8381fece980c838284e
Didlar <didriklarnholm265@gmail.com>
2022-02-16 15:07:00 +0100

    added post and get request

--- server/server.js
+++ server/server.js
@@ -7,4 +7,8 @@ app.get("/login", (req, res, next) =>{
     res.json({username: "Didlar", password: "lolleboien"})
 } );
 
+app.post("/login", (req, res) => {
+    res.sendStatus(401);
+})
+
 app.listen(3000)
